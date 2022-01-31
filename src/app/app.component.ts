f7e2fee54897f197a805d5d1b082959bd12d19d3
Postu Sebastian <postu.sebastian@gmail.com>
2022-01-31 16:50:25 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-second-angular-app';
+  newMemberName = ""
+  members: string[] = [];
+  errorMessage = '';
+  numberOfTeams: number | "" = "";
+  teams: string[][] = [];
+
+    onInput(member:string){
+      this.newMemberName= member;
+    }
+
+  onNumberOfTeamsInput(value: string){
+    this.numberOfTeams = Number(value);
+  }
+  addMember(){
+    if(!this.newMemberName){ 
+      this.errorMessage = "Name can't be empty";
+      return; }
+    this.members.push(this.newMemberName);
+    this.newMemberName = "";
+    this.errorMessage = "";
+  }
+  generateTeams(){
+    if(!this.numberOfTeams || this.numberOfTeams <= 0) { 
+      this.errorMessage = "Invalid number of teams";
+      return; }
+
+      if(this.members.length < this.numberOfTeams){
+        this.errorMessage = "Not enough members";
+        return;
+      }
+
+      this.errorMessage = "";
+    const newMembers = [...this.members];
+
+    while (newMembers.length){
+      for(let i=0; i<this.numberOfTeams; i++){
+        const randomIndex = Math.floor(Math.random() * newMembers.length);
+        const member = newMembers.splice(randomIndex,1)[0];
+        if(!member) { break; }
+        if(this.teams[i]){
+          this.teams[i].push(member);
+        }
+        else {
+          this.teams[i] = [member];
+        }
+      }
+    }
+    this.members = [];
+    this.numberOfTeams ="";
+    console.log(this.teams);
+  }
 }
+
